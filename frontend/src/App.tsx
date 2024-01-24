ff526d2a2ff848771f4f1f5813c5c0df5586a9e4
cameronbeattie306 <cameronbeattie306@gmail.com>
2024-01-23 19:40:58 -0600

    React Router implemented
    Nav bar might be interesting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+        To get started, edit <code>src/App.tsx</code> and save to reload.
+      </p>
     </div>
   );
 }

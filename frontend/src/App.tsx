a5b4b13993f75579aac70fc455230a2f073cdb1c
Cameron <cameronbeattie306@gmail.com>
2024-01-23 23:26:52 -0600

    fixed issue with page rendering on url change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import { Outlet } from "react-router";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.tsx</code> and save to reload.
-      </p>
+    // navbar component goes above outlet
+    <div>
+      <Outlet />
     </div>
   );
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,6 +8,7 @@ import router from "./Routes/Routes";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />

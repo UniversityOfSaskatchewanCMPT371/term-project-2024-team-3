e33b9a195360843140568093ecb60cb4c8af69ee
Eric Buell <redwing14e@gmail.com>
2024-01-25 21:14:15 -0600

    fixed test

--- frontend/src/HomePage/HomePage.test.tsx
+++ frontend/src/HomePage/HomePage.test.tsx
@@ -8,6 +8,7 @@ test("renders learn react link", () => {
   getByText("Accurately Process Your Fitness Data");
   getByText("How To Contribute Data");
   getByText(
-    "BEAP Engine is a research project developed by Dr. Daniel Fuller and the Built Environment and Active Populations (BEAP) Lab. The purpose of this study is to collect and analyze large volumes of Apple Watch and Fitbit data and develop methods to standardize across device. We provide you with a CSV file of your data and give you detailed information about sedentary behaviour, and moderate to vigorous activity based on our machine learning methods. We hope you will participate in our study.",
+    "BEAP Engine is a research project developed by Dr. Daniel Fuller",
+    { exact: false },
   );
 });

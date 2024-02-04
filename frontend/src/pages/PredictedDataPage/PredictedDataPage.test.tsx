bd3bebbe0ceb750f080bd3783bbef694ca2c3379
Daniel Benesh <dfb793@usask.ca>
2024-02-04 12:42:18 -0600

    used 'npm run lint -- --fix' to hopefully catch all format errors

--- frontend/src/pages/PredictedDataPage/PredictedDataPage.test.tsx
+++ frontend/src/pages/PredictedDataPage/PredictedDataPage.test.tsx
@@ -4,14 +4,16 @@ import PredictedDataPage from "./PredictedDataPage";
 
 test("Renders PredictedDataPage components", () => {
   const { getByText } = render(<PredictedDataPage />);
-  getByText("Step 3 - Predicted data files: " );
-  getByText("On this page, you can download your new data files with both the\
-            raw Apple Watch or Fitbit data, the features we use for our machine\
-            learning models, and the predicted activity for each minute of your\
-            data."
+  getByText("Step 3 - Predicted data files: ");
+  getByText(
+    "On this page, you can download your new data files with both the " +
+      "raw Apple Watch or Fitbit data, the features we use for our machine " +
+      "learning models, and the predicted activity for each minute of your " +
+      "data.",
   );
-  getByText("If you want to upload different files, go back to the file upload\
-            page. You can also click on individual steps to rerun a different\
-            machine learning model if you want."
+  getByText(
+    "If you want to upload different files, go back to the file upload " +
+      "page. You can also click on individual steps to rerun a different " +
+      "machine learning model if you want.",
   );
 });

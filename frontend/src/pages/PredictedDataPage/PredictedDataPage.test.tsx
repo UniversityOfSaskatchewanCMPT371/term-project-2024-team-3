6c4547e9f2a49e1635b111dd7d9c88d4fcdbd919
Daniel Benesh <dfb793@usask.ca>
2024-02-04 12:29:30 -0600

    removed 'getAllByText' in fnc def

--- frontend/src/pages/PredictedDataPage/PredictedDataPage.test.tsx
+++ frontend/src/pages/PredictedDataPage/PredictedDataPage.test.tsx
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import PredictedDataPage from "./PredictedDataPage";
 
 test("Renders PredictedDataPage components", () => {
-  const { getByText, getAllByText } = render(<PredictedDataPage />);
+  const { getByText } = render(<PredictedDataPage />);
   getByText("Step 3 - Predicted data files: " );
   getByText("On this page, you can download your new data files with both the raw\
             Apple Watch or Fitbit data, the features we use for our machine\

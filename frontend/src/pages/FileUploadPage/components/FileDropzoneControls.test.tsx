a99eb39cc529aaf9ea34b28c05c8496eb7d5e02d
seniorbaguette <102385296+seniorbaguette@users.noreply.github.com>
2024-02-07 22:00:56 -0600

    improved code quality

--- frontend/src/pages/FileUploadPage/components/FileDropzoneControls.test.tsx
+++ frontend/src/pages/FileUploadPage/components/FileDropzoneControls.test.tsx
@@ -3,15 +3,20 @@ import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import FileDropzoneControls from "./FileDropzoneControls";
 
+const onRadioChange = jest.fn();
 test("Renders FileDropzoneControls components", () => {
-  const { getByText } = render(<FileDropzoneControls />);
+  const { getByText } = render(
+    <FileDropzoneControls onRadioChange={onRadioChange} />,
+  );
   getByText("Fitbit");
   getByText("Apple Watch");
   getByText("Select File Type:");
 });
 
 test(" TID 1.7. Should be able to click Fitbit or Apple Watch radio", () => {
-  const { getByLabelText } = render(<FileDropzoneControls />);
+  const { getByLabelText } = render(
+    <FileDropzoneControls onRadioChange={onRadioChange} />,
+  );
   const fitbit = getByLabelText("Fitbit");
   const apple = getByLabelText("Apple Watch");
 
@@ -19,6 +24,7 @@ test(" TID 1.7. Should be able to click Fitbit or Apple Watch radio", () => {
   userEvent.click(fitbit);
   expect(fitbit).toBeChecked();
   expect(apple).not.toBeChecked();
+  expect(onRadioChange).toHaveBeenCalled();
 
   // Test clicking apple watch
   userEvent.click(apple);

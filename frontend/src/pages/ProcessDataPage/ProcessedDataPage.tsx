160b7c25ff5c622cbcdb1ac3cf75ea0290525eb3
Cameron <cameronbeattie306@gmail.com>
2024-02-21 14:18:43 -0600

    updated UI
    TODO:
    - make the render display files, their size, and uploaded time
    
    - fix vertical centering for elements in radial selector
    
    - add horizontal allignment for date added

--- frontend/src/pages/ProcessDataPage/ProcessedDataPage.tsx
+++ frontend/src/pages/ProcessDataPage/ProcessedDataPage.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-arrow-callback */
+
 import React from "react";
 import Rollbar from "rollbar";
 import {
@@ -8,7 +10,7 @@ import {
   Button,
   Alert,
   Snackbar,
-  List,
+  // List,
   Container,
 } from "@mui/material";
 
@@ -22,65 +24,122 @@ const ProcessedDataPage = function () {
   const rollbar = new Rollbar(rollbarConfig);
   rollbar.debug("Reached Processed Data page");
 
+  const getRendersOfFiles = (listOfFiles: Array<String>) => {
+    const renders = listOfFiles.map(function (i: String) {
+      console.log(i);
+      return (
+        <div className={styles.fileSelector}>
+          <FormControlLabel
+            value={i}
+            control={
+              <Radio
+                color="primary"
+                sx={{
+                  "&, &.Mui-checked": {
+                    color: "#5FCED3",
+                  },
+                }}
+              />
+            }
+            label={i}
+            labelPlacement="end"
+          />
+          <div className={styles.fileTextBox}>
+            <text className={styles.fileDate}>{i}</text>
+            <text className={styles.fileDate}>{i}</text>
+          </div>
+        </div>
+      );
+    });
+    return renders;
+  };
+
+  const renders = getRendersOfFiles([
+    "String 1",
+    "String 2",
+    "String 3",
+    "This is also a string",
+  ]);
+
   return (
     <div>
       <Container className={styles.containerDiv}>
-        <div className={styles.bannerinfo}>
-          <strong>Step 2 - Data prediction and download: </strong>
-          <span>In this step you can either download the</span>
-          <strong>.csv</strong>
-          <span>
-            of your Apple Watch or Fitbit data or, use our machine learning
-            methods to predict lying, sitting, and walking at difference
-            intensities. Select the file you want to predict, select the machine
-            learning model for the prediction (we recommend Random Forrest) and
-            click the
-          </span>
-          <strong> Predict File </strong>
-          <span>
-            button. Once prediction is complete, move to the predicted files
-            page.
-          </span>
-        </div>
         <div className={styles.action_bar}>
           <FormControl component="fieldset">
             <RadioGroup row defaultValue="svm">
               <FormControlLabel
                 value="svm"
-                control={<Radio color="primary" />}
+                control={
+                  <Radio
+                    color="primary"
+                    sx={{
+                      "&, &.Mui-checked": {
+                        color: "#5FCED3",
+                      },
+                    }}
+                  />
+                }
                 label="SVM"
                 labelPlacement="end"
               />
               <FormControlLabel
                 value="randomForest"
-                control={<Radio color="primary" />}
+                control={
+                  <Radio
+                    color="primary"
+                    sx={{
+                      "&, &.Mui-checked": {
+                        color: "#5FCED3",
+                      },
+                    }}
+                  />
+                }
                 label="Random Forest"
                 labelPlacement="end"
               />
               <FormControlLabel
                 value="decissionTree"
-                control={<Radio color="primary" />}
+                control={
+                  <Radio
+                    color="primary"
+                    sx={{
+                      "&, &.Mui-checked": {
+                        color: "#5FCED3",
+                      },
+                    }}
+                  />
+                }
                 label="Decission Tree"
                 labelPlacement="end"
               />
             </RadioGroup>
           </FormControl>
-
-          <Button variant="contained" className={styles.predictBtn}>
-            Predict File
-          </Button>
-          <Button variant="contained" className={styles.downloadBtn}>
-            Download File
-          </Button>
-          <Button
-            className={styles.goToPredicted}
-            variant="contained"
-            href="/PredictedDataPage"
-          >
-            Go To Predicted Files{" "}
-          </Button>
         </div>
-        <List className={styles.list} />
+        <div className={styles.columnDiv}>
+          <div className={styles.listDiv}>
+            <FormControl
+              component="fieldset"
+              className={styles.fileSelectorRad}
+            >
+              <RadioGroup>{renders}</RadioGroup>
+            </FormControl>
+          </div>
+          <div className={styles.buttonControl}>
+            <Button variant="contained" className={styles.predictBtn}>
+              Predict File
+            </Button>
+            <Button variant="contained" className={styles.downloadBtn}>
+              Download File
+            </Button>
+            <Button
+              className={styles.goToPredicted}
+              variant="contained"
+              href="/PredictedDataPage"
+            >
+              DELETE FILE{" "}
+            </Button>
+          </div>
+        </div>
       </Container>
 
       <Snackbar>

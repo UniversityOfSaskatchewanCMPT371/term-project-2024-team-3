d73b1e1098440a4d2b9a04c0baf01cad4f131c00
cameronbeattie306 <cameronbeattie306@gmail.com>
2024-02-27 13:59:16 -0600

    getting infinite re-renders when getting uploaded files

--- frontend/src/pages/ProcessDataPage/ProcessedDataPage.tsx
+++ frontend/src/pages/ProcessDataPage/ProcessedDataPage.tsx
@@ -10,15 +10,12 @@ import {
   Snackbar,
   Container,
 } from "@mui/material";
-import { WatchType } from "shared/api";
+import { DataType, PredictionType, RawFileData, WatchType } from "shared/api";
 import useListUploadedFiles from "shared/hooks/useListUploadedFiles";
-import { saveAs } from "file-saver";
-
+import usePredictedFile from "shared/hooks/usePredictFile";
+// import useDeleteFile from "shared/hooks/useDeleteFile";
+// import { saveAs } from "file-saver";
 import { FileWithPath } from "react-dropzone";
-
-// issues here
-import WatchService from "../../shared/Data";
-
 import styles from "./ProcessedDataPage.module.css";
 
 const ProcessedDataPage = function () {
@@ -29,17 +26,17 @@ const ProcessedDataPage = function () {
   const rollbar = new Rollbar(rollbarConfig);
   rollbar.debug("Reached Processed Data page");
 
-  const { uploadedFiles } = useListUploadedFiles(WatchType.APPLE_WATCH);
-  console.log(uploadedFiles);
-
   const [files, setFiles] = useState<any[]>([]);
   const [currentFile, setCurrentFile] = useState<any>();
-  const [selectedModel, setSelectedModel] = useState<string>("svm");
+  const [selectedModel, setSelectedModel] = useState<PredictionType>(
+    PredictionType.SVM,
+  );
 
-  const watchService = new WatchService();
+  const { handlePredict } = usePredictedFile();
+  // const { handleDelete } = useDeleteFile();
 
   // the list of radial selectors for the file list
-  let renders;
+  let renders: any;
 
   /**
    * This function will get the users processed files from the database
@@ -47,41 +44,31 @@ const ProcessedDataPage = function () {
    * POST-Conditions: Files will be updated to be the users list of files
    *                  The list of displayed files will be updated to show this
    */
-
-  // async
-  const getProcessedFiles = () => {
-    // await
-    watchService
-      .getProcessedDataList("fitbit")
-      .then(({ data, status }) => {
-        if (status !== 200) {
-          throw Error(data.message);
-        }
-        const transformData = [...data.list].map((file: File) => ({
+  const getUploadedFiles = () => {
+    if (files.length === 0) {
+      try {
+        const { uploadedFiles } = useListUploadedFiles(WatchType.APPLE_WATCH);
+        const transformData = uploadedFiles.map((file: RawFileData) => ({
           ...file,
-          watch: "Fitbit",
+          watch: DataType.APPLE_WATCH,
         }));
         setFiles([...files, ...transformData]);
-      })
-      .catch((err: Error) => {
-        rollbar.error(err);
-      });
-    // await
-    watchService
-      .getProcessedDataList("applewatch")
-      .then(({ data, status }) => {
-        if (status !== 200) {
-          throw Error(data.message);
-        }
-        const transformData = [...data.list].map((file: File) => ({
+      } catch (error: any) {
+        rollbar.error(error);
+        throw error;
+      }
+      try {
+        const { uploadedFiles } = useListUploadedFiles(WatchType.APPLE_WATCH);
+        const transformData = uploadedFiles.map((file: RawFileData) => ({
           ...file,
-          watch: "AppleWatch",
+          watch: DataType.APPLE_WATCH,
         }));
         setFiles([...files, ...transformData]);
-      })
-      .catch((err: Error) => {
-        rollbar.error(err);
-      });
+      } catch (error: any) {
+        rollbar.error(error);
+        throw error;
+      }
+    }
   };
 
   /**
@@ -89,70 +76,63 @@ const ProcessedDataPage = function () {
    * PRE-Conditions: A file is selected, and a prediction method is selected
    * POST-Conditions: Sends the files to the R repo and adds them to the database???
    */
-  const predictFiles = () => {
+  const predictFiles = async (event: React.MouseEvent) => {
+    event.preventDefault();
     const { id, watch } = currentFile;
     const lowerCaseWatch = watch.toLowerCase();
-    watchService
-      .predict(id, selectedModel, lowerCaseWatch)
-      .then((response: any) => {
-        if (response.status !== 200) {
-          throw new Error(response.data.message);
-        }
-      })
-      .catch((err: Error) => {
-        rollbar.error(err);
-      });
+
+    await handlePredict(id, selectedModel, lowerCaseWatch);
   };
 
-  /**
-   * creates a blob for a file for download
-   * @param b64Data the data to be downloaded
-   * @param contentType a string representing the type
-   * @param sliceSize the size of the slice for the blob
-   * @returns a blob for downloading
-   */
-  const b64toBlob = function (
-    b64Data: string,
-    contentType: string,
-    sliceSize: number,
-  ) {
-    // contentType = contentType || "";
-    // sliceSize = sliceSize || 512; // sliceSize represent the bytes to be process in each batch(loop), 512 bytes seems to be the ideal slice size for the performance wise
-    const byteCharacters = atob(b64Data);
-    const byteArrays = [];
-    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-      const slice = byteCharacters.slice(offset, offset + sliceSize);
-      const byteNumbers = new Array(slice.length);
-      for (let i = 0; i < slice.length; i += 1) {
-        byteNumbers[i] = slice.charCodeAt(i);
-      }
-      const byteArray = new Uint8Array(byteNumbers);
-      byteArrays.push(byteArray);
-    }
-    const blob = new Blob(byteArrays, { type: contentType });
-    return blob;
-  };
+  // /**
+  //  * creates a blob for a file for download
+  //  * @param b64Data the data to be downloaded
+  //  * @param contentType a string representing the type
+  //  * @param sliceSize the size of the slice for the blob
+  //  * @returns a blob for downloading
+  //  */
+  // const b64toBlob = function (
+  //   b64Data: string,
+  //   contentType: string,
+  //   sliceSize: number,
+  // ) {
+  //   // contentType = contentType || "";
+  //   // sliceSize = sliceSize || 512; // sliceSize represent the bytes to be process in each batch(loop), 512 bytes seems to be the ideal slice size for the performance wise
+  //   const byteCharacters = atob(b64Data);
+  //   const byteArrays = [];
+  //   for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+  //     const slice = byteCharacters.slice(offset, offset + sliceSize);
+  //     const byteNumbers = new Array(slice.length);
+  //     for (let i = 0; i < slice.length; i += 1) {
+  //       byteNumbers[i] = slice.charCodeAt(i);
+  //     }
+  //     const byteArray = new Uint8Array(byteNumbers);
+  //     byteArrays.push(byteArray);
+  //   }
+  //   const blob = new Blob(byteArrays, { type: contentType });
+  //   return blob;
+  // };
 
-  /**
-   * Downloads the currently selected file to the users computer
-   */
-  const downloadFile = () => {
-    const { id, watch } = currentFile;
-    const lowerCaseWatch = watch.toLowerCase();
-    watchService
-      .download(id, "process", lowerCaseWatch)
-      .then((response: any) => {
-        const blob = b64toBlob(
-          response.data.file,
-          "application/octet-stream",
-          512,
-        );
-        saveAs(blob, `${watch} ${id}.zip`);
-      })
-      .catch((err: Error) => {
-        rollbar.error(err);
-      });
-  };
+  // /**
+  //  * Downloads the currently selected file to the users computer
+  //  */
+  // const downloadFile = () => {
+  //   const { id, watch } = currentFile;
+  //   const lowerCaseWatch = watch.toLowerCase();
+  //   watchService
+  //     .download(id, "process", lowerCaseWatch)
+  //     .then((response: any) => {
+  //       const blob = b64toBlob(
+  //         response.data.file,
+  //         "application/octet-stream",
+  //         512,
+  //       );
+  //       saveAs(blob, `${watch} ${id}.zip`);
+  //     })
+  //     .catch((err: Error) => {
+  //       rollbar.error(err);
+  //     });
+  // };
 
   /**
    *  Maps the list of files to a list of radial selectors for the files list
@@ -187,19 +167,7 @@ const ProcessedDataPage = function () {
     });
   };
 
-  // // TEST DATA //
-  // const file = new File([], "../ProcessedDataPage.test.tsx");
-  // const file2 = new File([], "../ProcessedDataPage.tsx");
-  // const file3 = new File(
-  //   [],
-  //   "../../PredictedDataPage/PredoctedDataPage.module.css",
-  // );
-
-  // if (files.length === 0) {
-  //   setFiles([file, file2, file3]);
-  // }
-
-  getProcessedFiles();
+  getUploadedFiles();
 
   getRendersOfFiles();
 
@@ -211,7 +179,7 @@ const ProcessedDataPage = function () {
             <RadioGroup row defaultValue="svm">
               <FormControlLabel
                 value="svm"
-                onClick={() => setSelectedModel("svm")}
+                onClick={() => setSelectedModel(PredictionType.SVM)}
                 control={
                   <Radio
                     color="primary"
@@ -227,7 +195,7 @@ const ProcessedDataPage = function () {
               />
               <FormControlLabel
                 value="randomForest"
-                onClick={() => setSelectedModel("randomForest")}
+                onClick={() => setSelectedModel(PredictionType.RANDOM_FOREST)}
                 control={
                   <Radio
                     color="primary"
@@ -243,7 +211,7 @@ const ProcessedDataPage = function () {
               />
               <FormControlLabel
                 value="decissionTree"
-                onClick={() => setSelectedModel("decisionTree")}
+                onClick={() => setSelectedModel(PredictionType.DECISSION_TREE)}
                 control={
                   <Radio
                     color="primary"
@@ -280,7 +248,7 @@ const ProcessedDataPage = function () {
             <Button
               variant="contained"
               className={styles.downloadBtn}
-              onClick={downloadFile}
+              // onClick={downloadFile}
             >
               Download File
             </Button>

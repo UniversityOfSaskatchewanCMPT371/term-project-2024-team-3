de5f70c033041f490206070bbc1a766a172a7e5d
Ralph Gregorio <ralphlouiegregorio@gmail.com>
2024-02-27 20:46:03 -0600

    Fixed test by making custom renderer for rollbar provider

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { renderWithProvider } from "shared/util/tests/render";
 import Navbar from "./components/Navbar/Navbar";
 
 test(" TID 1.4. renders all links", () => {
-  render(
+  renderWithProvider(
     <Router>
       <Navbar />
     </Router>,

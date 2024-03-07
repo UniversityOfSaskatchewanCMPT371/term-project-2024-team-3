4fa87db2fa333917be531c5a5572b2ef64ec674b
Ralph Gregorio <ralphlouiegregorio@gmail.com>
2024-03-06 18:07:56 -0600

    Fix dependencies

--- backend/src/main/java/com/beaplab/tests/controller/HomeControllerTest.java
+++ backend/src/main/java/com/beaplab/tests/controller/HomeControllerTest.java
@@ -1,42 +1,24 @@
 package com.beaplab.tests.controller;
 
-import org.testng.annotations.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.beaplab.BeaplabEngine.controller.HomeController;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-class HomeControllerTest {
-
-    @InjectMocks
-    HomeController homeController;
-
-    @Mock
-    org.apache.log4j.Logger logger;
+public class HomeControllerTest {
 
     @Test
-    void testHandleRequest() throws Exception {
-        MockitoAnnotations.initMocks(this);
+    public void testHandleRequest() throws Exception {
+        HomeController controller = new HomeController();
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 
-        // Mocking behavior
-        String expectedMessage = "This message is returned from HomeController.";
-        ModelAndView expectedModelAndView = new ModelAndView("Index");
-        expectedModelAndView.addObject("message", expectedMessage);
-
-        // Method call
-        ModelAndView actualModelAndView = homeController.handleRequest();
-
-        // Verification
-        assertEquals(expectedModelAndView.getViewName(), actualModelAndView.getViewName());
-        assertEquals(expectedMessage, actualModelAndView.getModel().get("message"));
-
-        // Verify that logger.info() was called
-        verify(logger, times(1)).info("in HomeController");
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Index"))
+                .andExpect(model().attributeExists("message"))
+                .andExpect(model().attribute("message", "This message is returned from HomeController."));
     }
 }

85f561a22df3c97517646c1a248528f8e9ed79f4
glenntanjoh <glenntanjoh@gmail.com>
2024-03-11 19:08:23 -0600

    Updated Navbar playwright test

--- frontend/tests_end2end/Navbar.spec.ts
+++ frontend/tests_end2end/Navbar.spec.ts
@@ -42,10 +42,6 @@ test("Navbar Test", async ({ page }) => {
     await page.getByRole("link", { name: "LOGOUT" }).click();
     await expect(page).toHaveURL("./");
 
-    const LogOutPage = page.locator("#root");
-
-    await expect(LogOutPage).toContainText("Logging out...");
-
     // checking that the navbar can direct to the home page
     await page.getByRole("link", { name: "HOME" }).click();
     await expect(page).toHaveURL("./");

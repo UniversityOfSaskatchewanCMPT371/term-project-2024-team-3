592ae952a1692b6edec5da2074a6ffe3f3cc4f20
Ralph Gregorio <ralphlouiegregorio@gmail.com>
2024-03-13 00:04:43 -0600

    Created staging environment for playwright

--- frontend/tests_end2end/utils.ts
+++ frontend/tests_end2end/utils.ts
@@ -5,24 +5,10 @@ import { WatchType } from "shared/api";
 const setupMockLogin = async (page: Page): Promise<void> => {
     await page.goto("./login");
 
-    await page.route("**/loginuser", async (route) => {
-        const responseData = {
-            userID: 123,
-            Authorities: ["admin"],
-        };
-        await route.fulfill({
-            status: 200,
-            headers: {
-                "Content-Type": "application/json",
-                token: "mockToken",
-            },
-            body: JSON.stringify(responseData),
-        });
-    });
-
-    await page.fill("#username", "testuser");
-    await page.fill("#password", "testpassword");
+    await page.fill("#username", "hello");
+    await page.fill("#password", "123");
     await page.click('button[type="submit"]');
+    await page.waitForTimeout(1000);
 };
 
 const setupMockUpload = async (page: Page, type: WatchType): Promise<void> => {

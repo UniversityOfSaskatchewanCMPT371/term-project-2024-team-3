fd3ccc01e0dab35740c4bcbabf782da361840dca
Marcus Kruger <mpk467@usask.ca>
2024-03-13 22:38:01 -0600

    display file list element
    uses mocked data

--- frontend/src/pages/PredictedDataPage/PredictedDataPage.tsx
+++ frontend/src/pages/PredictedDataPage/PredictedDataPage.tsx
@@ -1,47 +1,121 @@
-import React, { useState } from "react";
-import { Button, Container, List } from "@mui/material";
+import React from "react";
+import {
+    Button,
+    Container,
+    FormControlLabel,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Radio,
+    RadioGroup,
+} from "@mui/material";
 import { useRollbar } from "@rollbar/react";
 import { FileData, WatchType } from "shared/api";
-import useGetPredictedDataList from "shared/hooks/useGetPredictedDataList";
+// import useGetPredictedDataList from "shared/hooks/useGetPredictedDataList";
+import moment from "moment";
 import styles from "./PredictedDataPage.module.css";
 
-type PredictedFileWithType = FileData & { watch: WatchType; name: String };
+type PredictedFileWithType = FileData & { watch: WatchType; name: String; isSelected: boolean };
 
 const PredictedDataPage = function () {
     const rollbar = useRollbar();
     rollbar.debug("Reached Predicted Data page");
 
-    const [availableFiles, setAvailableFilesDisplay] = useState<Array<PredictedFileWithType>>([]); // we need filetype (to know which service) and id
+    // const { uploadedFiles: fitBitFiles } = useGetPredictedDataList(WatchType.FITBIT); // mock this for testing
+    // const { uploadedFiles: appleWatchFiles } = useGetPredictedDataList(WatchType.APPLE_WATCH);
 
-    const { uploadedFiles: fitbitFiles } = useGetPredictedDataList(WatchType.FITBIT); // assume this works. Maybe we can mock it for test
-    const { uploadedFiles: appleWatchFiles } = useGetPredictedDataList(WatchType.APPLE_WATCH);
+    const fitBitFiles = [
+        {
+            id: 123,
+            data: new Uint8Array([1, 2]),
+            predictionType: null,
+            dateTime: new Date(),
+        },
+        {
+            id: 456,
+            data: new Uint8Array([5, 6]),
+            predictionType: null,
+            dateTime: new Date(),
+        },
+    ];
 
-    const appleWatchPredictedFiles =
+    const appleWatchFiles = [
+        {
+            id: 123,
+            data: new Uint8Array([1, 2]),
+            predictionType: null,
+            dateTime: new Date(),
+        },
+        {
+            id: 456,
+            data: new Uint8Array([5, 6]),
+            predictionType: null,
+            dateTime: new Date(),
+        },
+    ];
+
+    const appleWatchPredictedFiles: Array<PredictedFileWithType> =
         appleWatchFiles?.length !== 0
             ? appleWatchFiles.map((file: FileData) => ({
                   ...file,
                   watch: WatchType.APPLE_WATCH,
                   name: `${WatchType.APPLE_WATCH} ${file.id.toString()}`,
+                  isSelected: false,
               }))
             : [];
 
-    const fitbitPredictedFiles =
-        fitbitFiles?.length !== 0
-            ? fitbitFiles.map((file: FileData) => ({
+    const fitbitPredictedFiles: Array<PredictedFileWithType> =
+        fitBitFiles?.length !== 0
+            ? fitBitFiles.map((file: FileData) => ({
                   ...file,
                   watch: WatchType.FITBIT,
                   name: `${WatchType.FITBIT} ${file.id.toString()}`,
+                  isSelected: false,
               }))
             : [];
 
-    setAvailableFilesDisplay([
-        ...availableFiles,
-        ...fitbitPredictedFiles,
-        ...appleWatchPredictedFiles,
-    ]);
+    console.log(appleWatchPredictedFiles);
+    console.log(fitbitPredictedFiles);
 
-    const availableFilesDisplay = availableFiles.map((file) => <li>{file.name} </li>);
+    const availableFiles: Array<PredictedFileWithType> =
+        fitbitPredictedFiles.concat(appleWatchPredictedFiles);
 
+    const availableFilesDisplay =
+        availableFiles.length > 0 ? (
+            availableFiles.map((file) => (
+                <ListItem
+                    key={file.id.toString()}
+                    style={{ paddingTop: "0px", paddingBottom: "0px", cursor: "pointer" }}
+                    // onClick={ () => {file.isSelected = !file.isSelected} }
+                >
+                    <ListItemIcon>
+                        <RadioGroup>
+                            <FormControlLabel
+                                label="" // find out what this property is for
+                                value=""
+                                control={<Radio color="primary" />}
+                                labelPlacement="start"
+                                checked={file.isSelected}
+                            />
+                        </RadioGroup>
+                    </ListItemIcon>
+                    <ListItemText primary={`${file.watch} - ${file.id}`} />
+                    {file.dateTime ? (
+                        <ListItemText
+                            primary={moment(file.dateTime).format("ddd, D MMM YYYY, hh:mm:ss A")}
+                        />
+                    ) : (
+                        ""
+                    )}
+                </ListItem>
+            ))
+        ) : (
+            <li className={styles.list} style={{ marginTop: "15px" }}>
+                No files
+            </li>
+        );
+    console.log(availableFilesDisplay);
     return (
         <div>
             <Container className={styles.container}>
@@ -52,9 +126,6 @@ const PredictedDataPage = function () {
                         Watch or Fitbit data, the features we use for our machine learning models,
                         and the predicted activity for each minute of your data.
                     </span>
-                </div>
-                <div>
-                    <ul>{availableFilesDisplay}</ul>
                 </div>
                 <div className={styles.container}>
                     <h1>Files: </h1>
@@ -72,7 +143,10 @@ const PredictedDataPage = function () {
                         </Button>
                     </div>
                 </div>
-                <List className={styles.list} />
+
+                {/* Predicted Files List  */}
+                <List className={styles.list}>{availableFilesDisplay}</List>
+
                 <div className={styles.bannerinfo}>
                     <span>
                         If you want to upload different files, go back to the file upload page. You

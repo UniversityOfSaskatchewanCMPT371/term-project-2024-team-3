da0ea3cdfad52e50caa9167397e75a67f30d2db8
cameronbeattie306 <cameronbeattie306@gmail.com>
2024-03-22 13:48:02 -0600

    now tests the width of the bar

--- frontend/src/components/ProgressBar/ProgressBar.test.tsx
+++ frontend/src/components/ProgressBar/ProgressBar.test.tsx
@@ -25,11 +25,9 @@ test("TID 5.01. Renders ProgressBar component", () => {
     getByText("70%"); // correct percent in the bar
     getByText("This is a test message");
 
-    // const progressBar =
-    getByTestId("progressBarInner");
+    const progressBar = getByTestId("progressBarInner");
 
-    // expect(progressBar).toHaveAttribute("width", "70%")     // making sure the width is correct
-    // expect(progressBar.getAttribute("width")).toEqual("70%");    // cant seem to get this to work :(
+    expect(progressBar.style.width).toEqual("70%"); // making sure the width is correct
 
     const minButton = getByTestId("minimizeButton");
     if (minButton === null) {
@@ -78,4 +76,40 @@ test("TID 5.04. Does not render ProgressBar component with percentage > 100", ()
     );
 
     expect(container).toBeEmptyDOMElement();
+});
+
+test("TID 5.05. Renders ProgressBar component", () => {
+    authSpy.mockReturnValue({ isAuthenticated: true } as AuthReturnValue);
+    const { getByText, getByTestId } = renderWithProvider(
+        <ProgressBar percentage={5} message="This is a test message" isVisible />,
+    );
+
+    const popup = getByTestId("progressBarPopup"); // popup displays
+    getByText("5%"); // correct percent in the bar.
+    getByText("This is a test message");
+
+    const progressBar = getByTestId("progressBarInner");
+
+    expect(progressBar.style.width).toEqual("9%"); // making sure the width is correct. 9 is the minumum
+
+    const minButton = getByTestId("minimizeButton");
+    if (minButton === null) {
+        throw new Error("minButton did not render");
+    } else {
+        // press the minimize button
+        userEvent.click(minButton);
+        expect(popup).not.toBeVisible(); // should be hidden
+        const maxButton = getByTestId("maximizeButton");
+        if (maxButton === null) {
+            throw new Error("maxButton did not render");
+        } else {
+            // click the max button
+            expect(maxButton).toBeInTheDocument();
+            userEvent.click(maxButton);
+
+            expect(popup).toBeVisible(); // should be visible
+            getByText("5%"); // correct percent in the bar
+            getByText("This is a test message");
+        }
+    }
 });

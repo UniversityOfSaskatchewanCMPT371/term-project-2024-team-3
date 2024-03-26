b791b74499a33f96d7e1d3a0e0317582ec8881d4
Ralph Gregorio <ralphlouiegregorio@gmail.com>
2024-03-26 13:59:05 -0600

    remove smelly code

--- frontend/src/components/Icons/index.test.tsx
+++ frontend/src/components/Icons/index.test.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import { render } from "@testing-library/react";
 import FileIcon from ".";
 
-const onConfirmMock = jest.fn();
-const onCloseMock = jest.fn();
-
 describe("Icons", () => {
     it("T5.?? should render file icon", () => {
         const { getByTestId } = render(

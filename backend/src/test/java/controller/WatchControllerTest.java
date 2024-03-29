1e9a0ded089d313e38b10b40a4f349aa14543517
Cameron <cameronbeattie306@gmail.com>
2024-03-29 00:25:26 -0600

    process test written

--- backend/src/test/java/controller/WatchControllerTest.java
+++ backend/src/test/java/controller/WatchControllerTest.java
@@ -2,29 +2,11 @@ package controller;
 
 import com.beaplab.BeaplabEngine.authentication.SessionDetails;
 import com.beaplab.BeaplabEngine.constants.BeapEngineConstants;
-import com.beaplab.BeaplabEngine.controller.AccessGroupController;
 import com.beaplab.BeaplabEngine.controller.WatchController;
-import com.beaplab.BeaplabEngine.metadata.AccessGroupDto;
 import com.beaplab.BeaplabEngine.metadata.ProcessedDataDto;
-import com.beaplab.BeaplabEngine.metadata.UserDto;
-import com.beaplab.BeaplabEngine.model.AccessGroup;
-import com.beaplab.BeaplabEngine.model.User;
-import com.beaplab.BeaplabEngine.repository.AccessGroupDao;
-import com.beaplab.BeaplabEngine.repository.UserDao;
-import com.beaplab.BeaplabEngine.repository.base.BaseRepository;
-import com.beaplab.BeaplabEngine.service.AccessGroupService;
 import com.beaplab.BeaplabEngine.service.AppleWatchService;
 import com.beaplab.BeaplabEngine.service.FitbitService;
-import com.beaplab.BeaplabEngine.service.IncorrectLoginsService;
-import com.beaplab.BeaplabEngine.service.UserService;
-import com.beaplab.BeaplabEngine.util.error.UserAlreadyExistException;
-import com.beaplab.BeaplabEngine.util.objectMapper.AccessGroupMapper;
-import com.beaplab.BeaplabEngine.util.objectMapper.UserMapper;
 import com.rollbar.notifier.Rollbar;
-
-import javassist.bytecode.ByteArray;
-
-import org.springframework.web.multipart.MultipartFile;
 
 import org.json.simple.JSONObject;
 import org.junit.Before;
@@ -32,7 +14,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.*;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -40,14 +21,9 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.mock.web.MockMultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.InputStream;
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpSession;
-import javax.swing.tree.ExpandVetoException;
 
 import static org.junit.Assert.*;
 import static utils.MockFactory.*;
@@ -89,7 +65,6 @@ public class WatchControllerTest {
         expected = new ModelAndView("appleWatchFileUpload");
         result = watchController.getUploadView("applewatch");
         assertEquals(expected.getViewName(), result.getViewName());
-
     }
 
     /**
@@ -140,35 +115,27 @@ public class WatchControllerTest {
     }
 
     /**
-     * T5.44
+     * 
      */
     @Test
-    public void testUploadWithInvalidData() {
+    public void testProcessFiles() {
         JSONObject mockedReturn = new JSONObject();
         mockedReturn.put(BeapEngineConstants.SUCCESS_STR, true);
-        mockedReturn.put("raw_data_id", 123);
+        mockedReturn.put("processed_id", "processedDataId");
         mockedReturn.put("status_code", HttpStatus.OK.value());
-
-        // mocking request and session details
-        byte[] bytes = { 1, 2, 3 };
-        MockMultipartFile file = new MockMultipartFile("123", bytes);
-
-        MockMultipartHttpServletRequest request = new MockMultipartHttpServletRequest();
-        request.setSession(httpSession);
-        request.addFile(file);
 
         SessionDetails session = new SessionDetails();
         session.setUserId(1L);
 
-        when(appleWatchService.UploadAndPersist(request.getFileMap(), 1L)).thenReturn(mockedReturn);
-        when(fitbitService.UploadAndPersist(request.getFileMap(), 1L)).thenReturn(mockedReturn);
+        when(appleWatchService.processData(123L)).thenReturn(mockedReturn);
+        when(fitbitService.processData(123L)).thenReturn(mockedReturn);
 
         when(httpSession.getAttribute("SESSION_DETAILS")).thenReturn(session);
 
         /**
          * apple
          */
-        ResponseEntity<JSONObject> responseEntity = watchController.upload("applewatch", request);
+        ResponseEntity<JSONObject> responseEntity = watchController.processFiles("123", "applewatch");
 
         HttpStatus expectedStatus = HttpStatus.OK;
         HttpStatus resultStatus = responseEntity.getStatusCode();
@@ -179,19 +146,11 @@ public class WatchControllerTest {
         /**
          * fitbit
          */
-        responseEntity = watchController.upload("fitbit", request);
+        responseEntity = watchController.processFiles("123", "fitbit");
         resultStatus = responseEntity.getStatusCode();
 
         assertEquals(expectedStatus, resultStatus);
         assertEquals(mockedReturn, responseEntity.getBody());
-    }
-
-    /**
-     * 
-     */
-    @Test
-    public void testProcessFiles() {
-
     }
 
     /**

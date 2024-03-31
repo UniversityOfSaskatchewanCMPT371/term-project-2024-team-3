1544b08745e3b55f2057b7de56307e17cc2bfcf3
Daniel Benesh <dfb793@usask.ca>
2024-03-30 19:42:14 -0600

    adding back utils

---
+++ frontend/tests_end2end/utils.ts
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/prefer-default-export */
+import { Page } from "playwright";
+
+const setupLogin = async (page: Page): Promise<void> => {
+    await page.waitForTimeout(5000);
+    await page.goto("./login");
+
+    await page.fill("#username", "hello");
+    await page.fill("#password", "123");
+    await page.click('button[type="submit"]');
+    await page.waitForTimeout(5000);
+};
+
+export { setupLogin };

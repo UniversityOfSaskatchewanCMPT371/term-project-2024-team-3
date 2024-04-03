bea37e6a6004e443af6edd348918c47c657d22ea
Eric Buell <redwing14e@gmail.com>
2024-04-02 20:35:22 -0600

    fixed T2.6

--- frontend/tests_end2end/Navbar.spec.ts
+++ frontend/tests_end2end/Navbar.spec.ts
@@ -16,21 +16,28 @@ test("T2.6 Navbar Test", async ({ page }) => {
     const Title = page.locator("#root");
     await expect(Title).toContainText("BEAP ENGINE");
     const subTitle = page.locator("h6");
-    await expect(subTitle).toContainText("Accurately Process Your Fitness Data");
+    await expect(page.locator("h6")).toContainText("Unleashing The Power Of Your Fitness Data");
 
     // check the NavBar will direct users to the correct pages
     await page.getByRole("link", { name: "FILE UPLOAD" }).click();
-    await expect(page).toHaveURL("./FileUploadPage");
+    await expect(page).toHaveURL("./file-upload");
 
     await page.getByRole("link", { name: "PROCESSED FILES" }).click();
-    await expect(page).toHaveURL("./ProcessedDataPage");
+    await expect(page).toHaveURL("./processed-data");
 
     await page.getByRole("link", { name: "PREDICTED FILES", exact: true }).click();
-    await expect(page).toHaveURL("./PredictedDataPage");
-
-    const PredictedPage = page.locator("#root");
+    await expect(page).toHaveURL("./predicted-data");
 
     await page.getByTestId("profile").click();
+    await expect(page.getByRole("menuitem", { name: "My account" })).toBeVisible();
+    await expect(page.getByRole("menuitem", { name: "Logout" })).toBeVisible();
+    await page.getByRole("menuitem", { name: "My account" }).click();
+    await expect(page).toHaveURL("./profile");
+
+    await page.getByTestId("profile").click();
+    await expect(page.getByRole("menuitem", { name: "My account" })).toBeVisible();
+    await expect(page.getByRole("menuitem", { name: "Logout" })).toBeVisible();
+    await page.getByRole("menuitem", { name: "Logout" }).click();
     await expect(page).toHaveURL("./");
 
     // check that the user was actually logged out
